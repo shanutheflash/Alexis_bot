bb438e2087637d7593a496d17902cd870d4b5c29
Usman <30600533+Usman1329@users.noreply.github.com>
2018-07-06 19:31:42 +0500

    Update flip.py

--- tg_bot/modules/flip.py
+++ tg_bot/modules/flip.py
@@ -4,9 +4,9 @@ from telegram import Message, Chat, Update, Bot, MessageEntity
 from tg_bot import dispatcher
 
 @run_async
-def flip(bot: Bot, update: Update):
+def toss(bot: Bot, update: Update):
     message = update.effective_message
-    flip = bool(random.getrandbits(1))
+    toss = bool(random.getrandbits(1))
     
     if flip:
         message.reply_to_message.reply_text("Heads.")
@@ -14,5 +14,5 @@ def flip(bot: Bot, update: Update):
         message.reply_to_message.reply_text("Tails.")
     
 
-FLIP_HANDLER = CommandHandler("flip", flip)
-dispatcher.add_handler(FLIP_HANDLER)
+TOSS_HANDLER = CommandHandler("toss", toss)
+dispatcher.add_handler(TOSS_HANDLER)
